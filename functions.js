838e463ac500342c613b5eae194779ab6d682075
nasim-rbi <nasimrjweb3@gmail.com>
2022-09-19 08:19:59 -0700

    file added

--- functions.js
+++ functions.js
@@ -2,13 +2,6 @@ Number();
 setInterval();
 setTimeout();
 Boolean();
-<<<<<<< Updated upstream
-Boolean();
-split();
-map();
-split();
-map();
-=======
-split();
-map();
->>>>>>> Stashed changes
+
+
+
